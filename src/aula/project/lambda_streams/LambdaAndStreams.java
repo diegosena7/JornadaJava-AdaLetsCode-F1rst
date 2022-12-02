29bc778b2c663612dc59162115298ed7138bb9fc
diegosena7 <diego.tjdosantos@gmail.com>
2022-12-01 23:37:52 -0300

    usando lambda e streams

--- src/aula/project/lambda_streams/LambdaAndStreams.java
+++ src/aula/project/lambda_streams/LambdaAndStreams.java
@@ -1,22 +1,22 @@
 package aula.project.lambda_streams;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
  Uma função lambda é uma função sem declaração, isto é, não é necessário colocar um nome, um tipo de retorno e o modificador de acesso
  As funções lambda em Java tem a sintaxe definida como (argumento) -> (corpo)
  Expressões lambda basicamente expressam instâncias de interfaces funcionais (uma interface com método abstrato único é chamada de interface funcional)
+ BY: https://rinaldo.dev/java-8-streams-pare-de-usar-for-e-simplifique-seu-codigo/
 . */
 public class LambdaAndStreams {
 
     public static void main(String[] args) {
 
         List<Pessoa> listPessoas = Arrays.asList(new Pessoa("Eduardo", 50),
-                new Pessoa("Luiz", 32), new Pessoa("Bruna", 26), new Pessoa("Diego",33), new Pessoa("Ryan", 16));
+                new Pessoa("Luiz", 32), new Pessoa("Bruna", 26),
+                new Pessoa("Diego",33), new Pessoa("Ryan", 16));
         listPessoas.sort((p1,p2) -> p1.getNome().compareTo(p2.getNome()));
         System.out.println("Pessoas ordenadas por nome: " + listPessoas);
 
@@ -55,7 +55,7 @@ public class LambdaAndStreams {
 
         System.out.println();
         System.out.println("Imprimindo numeros pares:");
-        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 15, 20, 25);
         nums.stream().filter(num -> num % 2 == 0).forEach(num -> System.out.println(num));
 
         System.out.println();
@@ -73,5 +73,19 @@ public class LambdaAndStreams {
         list.stream()
                 .distinct()
                 .forEach(item -> System.out.println(item));
+
+        //Alterando a lista inicial e criando uma nova lista com Collectors.toList()
+        System.out.println();
+        listPessoas.stream().filter(p -> p.getIdade() > 30).collect(Collectors.toList()).forEach(pessoa -> System.out.println("Pessoa com mais de 20 anos: " + pessoa));
+
+        //Alterando a lista de numeros e transformando valores
+        Map<Boolean, List<Integer>> mapa = nums.stream().map(n -> n * 3).collect(Collectors.groupingBy(e -> e % 2 == 0));
+        System.out.println(mapa);
+
+        System.out.println();
+        //Busca o menor e maior valor da lista de numeros
+        Optional<Integer> min = nums.stream().min(Comparator.naturalOrder());
+        Optional<Integer> max = nums.stream().max(Comparator.naturalOrder());
+        System.out.println("Menor numero: " + min + " - Maior numero: " + max);
     }
 }
